8c9cbea5b48e9b504d36159c0eac907a411d39ba
Saichaitanya Reddy <saichaitanya338@gmail.com>
2018-11-06 21:09:13 +0530

    Create add.js
    adding two numbers using javascript

---
+++ Week_1/add.js
@@ -0,0 +1,19 @@
+var readline=require('readline-sync');
+var num1=readline.question("enter number one");
+var num2=readline.question("enter number two");
+
+var d=Number(num1)+Number(num2);
+
+console.log(d );
+
+-------------------------------------------------------
+
+
+F:\js>node add.js
+enter number one20
+enter number two30
+50
+
+
+
+
