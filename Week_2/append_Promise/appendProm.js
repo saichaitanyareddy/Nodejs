478ac7538a58b5fb0f8800fdb8f17f0b99266b96
Saichaitanya Reddy <saichaitanya338@gmail.com>
2018-11-17 21:53:01 +0530

    Updated

--- Week_2/append_Promise/appendProm.js
+++ Week_2/append_Promise/appendProm.js
@@ -1,13 +1,17 @@
 var fs=require('fs');
 
-const appendProm=(file1,file2)=>
+const p =(file1,file2)=>
 	{
 		return new Promise((resolve,reject)=>
 		{
-			fs.readFile(file2,'utf-8',function(err,data){fs.appendFile(file1,data,()=>{});});
+			fs.readFile(file2,'utf-8',function(err,data){ 
+									if(err){reject(err);}
+									
+									resolve(fs.appendFile(file1,data,()=>{}));});
 		});
 		
 	}	
+
 
 
 file1='file1.txt';
@@ -15,7 +19,7 @@ file2='file2.txt';
 file3='file3.txt';
 file4='file4.txt';
 file5='file5.txt';
-appendProm(file1,file2).then(appendProm(file1,file3)).then(appendProm(file1,file4)).then(appendProm(file1,file5)).catch(error=>console.log(error));
+p(file1,file2).then(p(file1,file3)).then(p(file1,file4)).then(p(file1,file5)).catch(error=>console.log(error));
 console.log("files appended");
 ------------------------------------------------------------------------------------------------------------------------------------------------------
 
